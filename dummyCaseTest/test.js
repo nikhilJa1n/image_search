5bc83c6fea20fa3858dec2f3c304bd4842a5c324
grepnikhil <nikhilj50@gmail.com>
2020-05-01 23:52:02 +0530

    add dummy test case

---
+++ dummyCaseTest/test.js
@@ -0,0 +1,4 @@
+const searchByImage = require('../server.js')
+
+searchByImage('https://cdn.vox-cdn.com/thumbor/8tLchaDMIEDNzUD3mYQ7v1ZQL84=/0x0:2012x1341/920x613/filters:focal(0x0:2012x1341):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg')
+    .then(console.log);
